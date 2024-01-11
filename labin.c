335e78a0369415b22f88b9db8ab19cced72d1a8f
amikula <152652831+kroro@users.noreply.github.com>
2024-01-11 12:13:55 +0100

    allocate shared memory

--- labin.c
+++ labin.c
@@ -65,6 +65,36 @@ static void randname(char *buf) {
 	}
 }
 
+static int create_shm_file(void) {
+	int retries = 100;
+	do {
+		char name[] = "/wl_shm-XXXXXX";
+		randname(name + sizeof(name) - 7);
+		--retries;
+		int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd >= 0) {
+			shm_unlink(name);
+			return fd;
+		}
+	} while (retries > 0 && errno == EEXIST);
+	return -1;
+}
+
+int allocate_shm_file(size_t size) {
+	int fd = create_shm_file();
+	if (fd < 0)
+		return -1;
+	int ret;
+	do {
+		ret = ftruncate(fd, size);
+	} while (ret < 0 && errno == EINTR);
+	if (ret < 0) {
+		close(fd);
+		return -1;
+	}
+	return fd;
+}
+
 int max(int a, int b) {
    return (a > b) ? a : b;
 }
